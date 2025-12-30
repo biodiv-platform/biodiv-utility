08d6dbd7e0b9fb54fffd83ce1762994b620aa13c
mekala.ravi <mekala.ravi@strandls.com>
2025-12-30 12:27:11 +0530

    Fixed left lines

--- src/main/java/com/strandls/utility/service/impl/UtilityServiceImpl.java
+++ src/main/java/com/strandls/utility/service/impl/UtilityServiceImpl.java
@@ -2155,7 +2155,7 @@ public class UtilityServiceImpl implements UtilityService {
 				cs.beginText();
 				cs.setFont(leftText.startsWith("*") ? boldFont : font, fontSize);
 				cs.newLineAtOffset(speciesField ? MARGIN + 25 : MARGIN + 15, currentYPos + 1.5f);
-				cs.showText(leftText.startsWith("*") ? leftLines.get(0).substring(1) : leftLines.get(0));
+				cs.showText(leftText.startsWith("*") ? leftLines.get(0) : leftLines.get(0));
 				cs.endText();
 			}
 
